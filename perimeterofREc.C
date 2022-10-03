77917304aae02f1c96f31aaf946cbd23b1c334d5
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-03 22:00:25 +0530

    Add files via upload

---
+++ perimeterofREc.C
@@ -0,0 +1,23 @@
+// write a program to calculate perimetr of rectangle:
+
+#include<stdio.h>
+
+int main()
+{
+ 
+            int a,b;
+    
+            printf("enter a");
+ 
+            scanf("%d",&a);
+ 
+            printf("enter b");
+  
+            scanf("%d",&b);
+   
+            printf("perimeter is: %d", 2*(a+b));
+  
+
+return 0;
+
+}
